e17e92d06db2d28823001169c3f72c5cdcfb9cf0
Aebram Laja <aebram.laja@iconwireless.ph>
2019-03-04 09:12:01 +0800

    Separate bootstrap components for loading

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,8 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+    require('./bootstrap-assets');
+    // require('bootstrap');
 } catch (e) {}
 
 /**
